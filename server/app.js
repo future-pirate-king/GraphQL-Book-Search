9bba0801a1bb0456dea439d96ab232e2d3f035de
Sam David <samd948@gmail.com>
2018-07-24 23:42:54 +0530

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schemas/schema');
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
+
+app.listen(4000, () => console.log('Listening on Port 4000...'));
