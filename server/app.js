c638f758842ef9ac4949f5c4573e9290eaeac731
Sam David <samd948@gmail.com>
2018-07-25 11:50:16 +0530

    enabled graphiql

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const schema = require('./schemas/schema');
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  schema
+  schema,
+  graphiql: true
 }));
 
 app.listen(4000, () => console.log('Listening on Port 4000...'));
