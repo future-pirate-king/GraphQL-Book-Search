807f6c999c65e167838d749ae94d680be29e726c
Sam David <samd948@gmail.com>
2018-07-25 23:39:08 +0530

    retrive books and map it to booklist

--- client/src/components/book-list.js
+++ client/src/components/book-list.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const BooksQuery = gql`
+  {
+    books {
+      id
+      name
+    }
+  }
+`;
 
 class BookList extends Component {
 
+  displayBooks = () => {
+    const { data } = this.props;
+
+    if (data.loading === true) {
+      return <strong>Please wait loading...</strong>;
+    }
+
+    return (
+      <ul>
+        {
+          data.books.map(book => (
+            <li key={book.id}>{book.name}</li>
+          ))
+        }
+      </ul>
+    );
+  }
+
   render() {
+
     return (
       <div>
-        <ul>
-          <li>Book name</li>
-        </ul>
+        { this.displayBooks() }
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(BooksQuery)(BookList);
