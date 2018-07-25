21865a6f7a2026109d59f2e72c8d8ba7ad77ce50
Sam David <samd948@gmail.com>
2018-07-25 12:50:47 +0530

    established connection to database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schemas/schema');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://sam:samd948@ds147181.mlab.com:47181/book-search', {
+  useNewUrlParser: true
+});
+mongoose.connection.once('open', () => console.log('Connected to db...'));
 
 app.use('/graphql', graphqlHTTP({
   schema,
