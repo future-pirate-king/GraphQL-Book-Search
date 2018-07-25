fd244dd0a7a5209bfbcb3855a7105f9c764e76f3
Sam David <samd948@gmail.com>
2018-07-25 21:19:13 +0530

    Added cors

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schemas/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb://sam:samd948@ds147181.mlab.com:47181/book-search', {
   useNewUrlParser: true
