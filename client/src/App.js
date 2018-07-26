7b984df329b2da0815385289e6bf1109e464d0d6
Sam David <samd948@gmail.com>
2018-07-26 12:21:47 +0530

    added add-book component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import BookList from './components/book-list';
+import AddBook from './components/add-book';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -15,6 +16,7 @@ class App extends Component {
         <div className="App">
           <h1>Sam's Book Store</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
---
+++ client/src/components/add-book.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getAuthorQuery = gql`
+  {
+    authors {
+      id
+      name
+    }
+  }
+`;
+
+class AddBook extends Component {
+
+  state = {};
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  displayAuthors = () => {
+    const { data } = this.props;
+
+    if (data.loading === true) {
+      return <option>loading...</option>;
+    }
+
+    return (data.authors.map(author => (
+      <option key={author.id} value={author.id}>{author.name}</option>
+    )));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h3>Add Book</h3>
+        <div>
+          <label htmlFor="bookName">
+            Name:<input id="bookName" type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="bookGenre">
+            Genre:<input id="bookGenre" type="text" name="genre" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="bookAuthor">
+            Author:
+            <select id="bookAuthor" name="author">
+              <option>Select</option>
+              { this.displayAuthors() }
+            </select>
+          </label>
+        </div>
+        <div>
+          <button onClick={this.handleSubmit}>+</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default graphql(getAuthorQuery)(AddBook);
